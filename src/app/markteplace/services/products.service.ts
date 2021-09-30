89b8af942710ff979dd04ed5e18aee5bda34a292
Estuardo-Sazo <estuardosazo1206@gmail.com>
2021-09-30 11:55:11 -0600

    discount product list basic design

--- src/app/markteplace/services/products.service.ts
+++ src/app/markteplace/services/products.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.mock';
+import { Product } from '../model/product';
 const urlApi =environment.mockapi;
-@Injectable({
-  providedIn: 'root',
-})
-export class CategoriesService {
-  constructor(private http: HttpClient) {}
 
-  getCategories() {
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
     return new Promise((resolve) => {
 
       if (urlApi) {
         //TODO: make local JSON file requests
-        this.http.get('./shared/guards/mocks/categories.json').subscribe((resp) => {
-          resolve(resp);
+        this.http.get('./shared/guards/mocks/products.json').subscribe((resp: Product[]) => {
+          resolve(resp.filter(product => product.oldPrice>0));
         });
       } else {
         //TODO: make requests to the API server
