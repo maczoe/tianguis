673741bf6a432b0c98e891d7b774643d1835b061
estuardo-sazo <estuardosazo1206@gmail.com>
2022-10-24 23:30:04 -0600

    Changing intro window styles

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonicSlides } from '@ionic/angular';
 import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IntroPage implements OnInit {
-
+  slides=[
+    {
+      title: "Compra",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium unde earum tenetur doloremque praesentium quia sequi velit quasi neque, eligendi minus, beatae fuga atque, mollitia repudiandae? Fuga, molestiae necessitatibus!"
+    },
+    {
+      title: "Vende",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium unde earum tenetur doloremque praesentium quia sequi velit quasi neque, eligendi minus, beatae fuga atque, mollitia repudiandae? Fuga, molestiae necessitatibus!"
+    },
+    {
+      title: "Cotiza",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium unde earum tenetur doloremque praesentium quia sequi velit quasi neque, eligendi minus, beatae fuga atque, mollitia repudiandae? Fuga, molestiae necessitatibus!"
+    },
+    {
+      title: "Emprende",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laudantium unde earum tenetur doloremque praesentium quia sequi velit quasi neque, eligendi minus, beatae fuga atque, mollitia repudiandae? Fuga, molestiae necessitatibus!"
+    }
+    
+  ]
   slideOpts = {
     initialSlide: 0,
     speed: 400
