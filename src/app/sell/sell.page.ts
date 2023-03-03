7cfa8a177e44edc466432785213fef29c8ec4dff
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-03-03 16:56:03 -0600

    add creation of product

--- src/app/sell/sell.page.ts
+++ src/app/sell/sell.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../markteplace/model/category';
+import { Product } from '../markteplace/model/product';
+import { CategoriesService } from '../markteplace/services/categories.service';
 
 @Component({
   selector: 'app-sell',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sell.page.scss'],
 })
 export class SellPage implements OnInit {
-  categories: string[] =[];
-  constructor() { }
+  categories: Category[] = [];
+  categoriesSelect = [];
+  product: Product={};
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit() {
+    this.categoriesService.getCategories().subscribe((data) => {
+      console.log(data);
+
+      this.categories = data;
+      console.log(this.categories);
+    });
   }
 
+  handleChange(ev) {
+    this.product.categories = ev.target.value;
+  }
+
+  onSubmit() {
+    console.log(this.product);
+  }
 }
