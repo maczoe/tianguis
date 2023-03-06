71786c94b244c20d0607b409f8f864adca88572f
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-03-06 17:05:56 -0600

    Changed Page of Reponses

--- src/app/quote/quote.page.ts
+++ src/app/quote/quote.page.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { LoadingController, ModalController } from '@ionic/angular';
 import { QuotesService } from '../markteplace/services/quotes.service';
 import { NewQuotePage } from '../modals/new-quote/new-quote.page';
 
 @Component({
   selector: 'app-quote',
   templateUrl: 'quote.page.html',
-  styleUrls: ['quote.page.scss']
+  styleUrls: ['quote.page.scss'],
 })
-export class QuotePage implements OnInit  {
+export class QuotePage implements OnInit {
   quotes = [];
-  constructor(private router: Router,
-              private quotesService: QuotesService,
-  ) { }
+  constructor(
+    private router: Router,
+    private quotesService: QuotesService,
+    private loadingCtrl: LoadingController
+  ) {
+    this.getQuote();
+  }
 
-  ngOnInit() {
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Obteniendo datos..',
+      //duration: 3000,
+    });
+
+    loading.present();
+  }
+
+  async finishLoading() {
+    return await this.loadingCtrl.dismiss();
+}
+
+  ngOnInit() {}
+
+  getQuote() {
+    //this.showLoading();
     this.quotesService.getQuotes().subscribe((data) => {
       this.quotes.push(data);
+     // this.finishLoading();
     });
-   }
-
+  }
 
   newQuote() {
     this.router.navigateByUrl('/new-quote');
