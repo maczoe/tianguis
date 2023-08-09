a5db71e1555101bf5d35d78995f11384d3c53b18
Estuardo-Sazo <estuardosazo1206@gmail.com>
2023-08-09 12:14:48 -0600

    Change of temporary chat view

--- src/app/markteplace/pages/chat-list/chat-list.page.ts
+++ src/app/markteplace/pages/chat-list/chat-list.page.ts
@@ -13,22 +13,22 @@ export class ChatListPage implements OnInit {
   image: string;
   product: Product = {};
   messages = [
-    /* {
+    {
       user: 'Mario Lopez',
-      createdAt: new Date('2022-12-2 14:01').getTime(),
+      createdAt: new Date('20223-12-2 14:01').getTime(),
       message: ' Hola !',
     },
     {
       user: 'Mario Lopez',
-      createdAt: new Date('2022-12-2 14:05').getTime(),
+      createdAt: new Date('2023-12-2 14:05').getTime(),
       message: ' Quisiera información de producto.',
     },
     {
       user: 'Simon Perez',
-      createdAt: new Date('2022-12-2 14:10').getTime(),
+      createdAt: new Date('2023-12-2 14:10').getTime(),
       message:
         'Un gusto saludarte, claro que si! que es lo que más te interesa del producto.',
-    }, */
+    },
   ];
 
   currentUser = 'Simon Perez';
