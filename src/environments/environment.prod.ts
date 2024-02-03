27a7a0c899c295b4be984bd3d033ae31cc74930c
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-02-03 09:52:58 -0600

    Fixed, chat list, get profile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,21 @@
 export const environment = {
-  production: true,
+  production: true /*  */,
   mockapi: false,
-  clientIdGoolge: '426633935974-63tc30cbqp0elpodm40c7i4ouf7b3h90.apps.googleusercontent.com',
-  privateKeyFirebase:
-  // eslint-disable-next-line max-len
-  '',
-  urlapi: 'http://34.16.48.193:3000/',
-  urlapiNoti: 'http://34.16.48.193:3001',
-  urlapiChat: 'http://34.16.48.193:3003',
-  //urlapi: 'http://192.168.0.175:3000/',
-  //urlapiNoti: 'http://192.168.0.175:3001',
-  //urlapiChat: 'http://192.168.0.175:3003',
+  clientIdGoolge:
+    '426633935974-63tc30cbqp0elpodm40c7i4ouf7b3h90.apps.googleusercontent.com', //WEB
+  firebase: {
+    apiKey: 'AIzaSyDq8MMgeoIRmk3HNimWL1ynnkIogovZxoA',
+    authDomain: 'tianguis-app-71868.firebaseapp.com',
+    projectId: 'tianguis-app-71868',
+    storageBucket: 'tianguis-app-71868.appspot.com',
+    messagingSenderId: '597171857974',
+    appId: '1:597171857974:web:956a895f7c15e53c2cc697',
+    measurementId: 'G-QBXGFK0355',
+  },
+ //urlapi: 'http://34.16.48.193:3000/',
+  //urlapiNoti: 'http://34.16.48.193:3001',
+  //urlapiChat: 'http://34.16.48.193:3003',
+  urlapi: 'https://api.tianguis.app:3000/',
+  urlapiNoti: 'https://api.tianguis.app:3001',
+  urlapiChat: 'https://api.tianguis.app:3003',
 };
